0dfe182a8fc11a5b5a19f34bb7a9bf0b86d6efc2
mrqamman <allensh@btinternet.com>
2018-08-11 21:34:56 +0100

    trying to make it build

--- hello-cloud.go
+++ hello-cloud.go
@@ -3,5 +3,5 @@ package main
 import "fmt"
 
 func main () {
-fmt.Println("Hello cloud - I've made a change again - and again - and again - stop saying again")
+fmt.Println("Hello cloud - I've made a change again - and again - stop saying again")
 }
